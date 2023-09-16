40abf6cf8afa0ecc2d68b9bc0317aca27a804f8c
ianrowe12 <87292868+ianrowe12@users.noreply.github.com>
2023-09-15 20:00:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBoaXZaSVTsP6P69_VApjjvT-jHhM0NXWo",
+    authDomain: "cafeteria-st-paul.firebaseapp.com",
+    databaseURL: "https://cafeteria-st-paul-default-rtdb.firebaseio.com",
+    projectId: "cafeteria-st-paul",
+    storageBucket: "cafeteria-st-paul.appspot.com",
+    messagingSenderId: "467227612415",
+    appId: "1:467227612415:web:7e951637edf623953e68ca",
+    measurementId: "G-SQLJEW26WG"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
